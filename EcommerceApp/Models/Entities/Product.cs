11c38155e5621455ad6cfdd77772be3e083062d0
codewithjoyal <codewithjoyal@gmail.com>
2026-06-17 20:58:36 +0530

    Add project files.

---
+++ EcommerceApp/Models/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace EcommerceApp.Models.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Range(0.01, 999999)]
+        public decimal Price { get; set; }
+
+        public int Stock {  get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
