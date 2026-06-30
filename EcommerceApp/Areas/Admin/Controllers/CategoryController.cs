08c06bdba1b86763955316b397b24f71fe45d46a
codewithjoyal <codewithjoyal@gmail.com>
2026-06-30 21:16:54 +0530

    edit page and option updated

--- EcommerceApp/Areas/Admin/Controllers/CategoryController.cs
+++ EcommerceApp/Areas/Admin/Controllers/CategoryController.cs
@@ -37,7 +37,41 @@ namespace EcommerceApp.Areas.Admin.Controllers
             if (ModelState.IsValid){
                 _context.Categories.Add(category);
                 _context.SaveChanges();
-                return RedirectToAction("Index");
+                TempData["Success"] = "Category created succesfully.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id) 
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category) {
+            if (!ModelState.IsValid)
+            {
+                foreach (var item in ModelState)
+                {
+                    foreach (var error in item.Value.Errors)
+                    {
+                        Console.WriteLine($"{item.Key} : {error.ErrorMessage}");
+                    }
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                TempData["Success"] = "Category updated succesfully.";
+                return RedirectToAction(nameof(Index));
             }
             return View(category);
         }
