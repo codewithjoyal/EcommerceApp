6801ef1aa8ca3eb5e17e501d78384e384c6d44f4
codewithjoyal <codewithjoyal@gmail.com>
2026-07-16 16:31:05 +0530

    changes updated

---
+++ EcommerceApp/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using EcommerceApp.Data;
+using EcommerceApp.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products
+                .Include(p => p.Category)
+                .ToList();
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product product)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public IActionResult DeletePost(int id)
+        {
+            return View();
+        }
+    }
+}
