2e8da71d9a066a51703f3fc4fcf3a42bee88cce7
codewithjoyal <codewithjoyal@gmail.com>
2026-07-16 16:31:29 +0530

    changes updated

--- EcommerceApp/Data/ApplicationDbContext.cs
+++ EcommerceApp/Data/ApplicationDbContext.cs
@@ -12,5 +12,13 @@ namespace EcommerceApp.Data
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+        }
     }
+
 }
