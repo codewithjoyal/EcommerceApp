fa0c74fc623ec5275c19dfd2084cfbc5af29e219
codewithjoyal <codewithjoyal@gmail.com>
2026-07-29 14:19:47 +0530

    user viewing product updated and product listing page(srp) created

---
+++ EcommerceApp/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using EcommerceApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products
+                .Include(p => p.Category)
+                .ToList();
+            return View(products);
+        }
+    }
+}
