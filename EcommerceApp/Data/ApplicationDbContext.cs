15dd231b8b0164acdf9e66c2b48e5475943a5ca3
codewithjoyal <codewithjoyal@gmail.com>
2026-08-02 21:08:07 +0530

    AddShoppingCart table created for cart.

--- EcommerceApp/Data/ApplicationDbContext.cs
+++ EcommerceApp/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace EcommerceApp.Data
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
---
+++ EcommerceApp/Models/Entities/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceApp.Models.Entities
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(ApplicationUserId))]
+        public IdentityUser? ApplicationUser { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [ForeignKey(nameof(ProductId))]
+        public Product? Product { get; set; }
+
+        [Range(1,100)]
+        public int Count { get; set; }
+    }
+}
