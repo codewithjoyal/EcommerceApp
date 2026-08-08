943636579d1f1783aba268cf431f3625e627bca0
codewithjoyal <codewithjoyal@gmail.com>
2026-08-08 11:02:21 +0530

    Add to cart implimented

--- EcommerceApp/Controllers/ProductController.cs
+++ EcommerceApp/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
 ﻿using EcommerceApp.Data;
+using EcommerceApp.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,9 +10,11 @@ namespace EcommerceApp.Controllers
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public ProductController(ApplicationDbContext context)
+        private readonly UserManager<IdentityUser> _userManager;
+        public ProductController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -25,6 +30,36 @@ namespace EcommerceApp.Controllers
                 return NotFound();
             }
             return View(product);
+        }
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddToCart(int productId) 
+        {
+            var userId = _userManager.GetUserId(User);
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Challenge();
+            }
+            var cartItem = _context.ShoppingCarts.FirstOrDefault
+                (c => c.ApplicationUserId == userId && c.ProductId == productId);
+            if (cartItem == null)
+            {
+                var shoppingCart = new ShoppingCart
+                {
+                    ApplicationUserId = userId,
+                    ProductId = productId,
+                    Count = 1
+                };
+                _context.ShoppingCarts.Add(shoppingCart);
+            }
+            else
+            {
+                cartItem.Count++;
+            }
+            _context.SaveChanges();
+            TempData["Success"] = "Product added to cart.";
+            return RedirectToAction(nameof(Index));
         }
     }
 }
